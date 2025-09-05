752cafd1ab5403e1186b4acf0af4737ef899af6f
Sergei Fomin <xoma686@protonmail.com>
2025-09-05 20:59:31 +0100

    SQLCPP-31: Solution to 02-1

---
+++ 02/1.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Genres (
+  IdGenre int PRIMARY KEY CHECK (IdGenre > 0),
+  Name varchar(255) NOT NULL
+);
+
+CREATE TABLE Artists (
+  IdArtist int PRIMARY KEY CHECK (IdArtist > 0),
+  FullName varchar(255) NOT NULL,
+  NickName varchar(255) NOT NULL
+);
+
+CREATE TABLE Albums (
+  IdAlbum int PRIMARY KEY CHECK (IdAlbum > 0),
+  Name varchar(255) NOT NULL,
+  ReleaseDate int NOT NULL CHECK (ReleaseDate > 0)
+);
+
+CREATE TABLE CompilationAlbums (
+  IdCompilationAlbum int PRIMARY KEY CHECK (IdCompilationAlbum > 0),
+  Name varchar(255) NOT NULL,
+  ReleaseDate int NOT NULL CHECK (ReleaseDate > 0)
+);
+
+CREATE TABLE Songs (
+  IdSong int PRIMARY KEY CHECK (IdSong > 0),
+  Name varchar(255) NOT NULL,
+  Duration int NOT NULL CHECK (Duration > 0)
+);
+
+CREATE TABLE ArtistGenres (
+  IdArtist int,
+  IdGenre int,
+  PRIMARY KEY (IdArtist, IdGenre),
+  FOREIGN KEY (IdArtist) REFERENCES Artists(IdArtist),
+  FOREIGN KEY (IdGenre) REFERENCES Genres(IdGenre)
+);
+
+CREATE TABLE AlbumArtists (
+  IdAlbum int,
+  IdArtist int,
+  PRIMARY KEY (IdAlbum, IdArtist),
+  FOREIGN KEY (IdAlbum) REFERENCES Albums(IdAlbum),
+  FOREIGN KEY (IdArtist) REFERENCES Artists(IdArtist)
+);
+
+CREATE TABLE AlbumSongs (
+  IdAlbum int,
+  IdSong int,
+  PRIMARY KEY (IdAlbum, IdSong),
+  FOREIGN KEY (IdAlbum) REFERENCES Albums(IdAlbum),
+  FOREIGN KEY (IdSong) REFERENCES Songs(IdSong)
+);
+
+CREATE TABLE CompilationAlbumSongs (
+  IdCompilationAlbum int,
+  IdSong int,
+  PRIMARY KEY (IdCompilationAlbum, IdSong),
+  FOREIGN KEY (IdCompilationAlbum) REFERENCES CompilationAlbums(IdCompilationAlbum),
+  FOREIGN KEY (IdSong) REFERENCES Songs(IdSong)
+);
